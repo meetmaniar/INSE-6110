18b02c68054986c8a3afa5cf604860157c57a703
Maniar Meet <meetrmaniar@gmail.com>
2018-11-11 18:17:21 -0500

    Add files via upload

--- cypher/main.java
+++ cypher/main.java
@@ -7,11 +7,14 @@ public class main {
 
 	public static void main(String[] args) {
 
-System.out.println(caeser.decrypt(substitution.decrypt(substitution.encrypt(caeser.encrypt("Meet", 5))),5))	;
+//System.out.println(caeser.decrypt(substitution.decrypt(substitution.encrypt(caeser.encrypt("Meet", 5))),5))	;
 
 	
+	System.out.println(substitution.key);
+	System.out.println(substitution.decrypt(substitution.key));
 	
-
+	System.out.println("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	System.out.println(substitution.encrypt("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
 }
 
 }
--- cypher/substitution.java
+++ cypher/substitution.java
@@ -1,14 +1,13 @@
 package cypher;
 
 public class substitution {
-	   final static String key = "]kYV}(!7P$n5_0i R:?jOWtF/=-pe'AD&@r6%ZXs\"v*N"
-	            + "[#wSl9zq2^+g;LoB`aGh{3.HIu4fbK)mU8|dMET><,Qc\\C1yxJ";
+	   final static String key = "@r6%ZXs\"v*N[#wSl9zq2^+g;Lo";
 	 	  
 	    static String encrypt(String s) {
 	        StringBuilder sb = new StringBuilder(s.length());
 	 
 	        for (char c : s.toCharArray())
-	            sb.append(key.charAt((int) c - 32));
+	            sb.append(key.charAt((int) c - 65));
 	 
 	        return sb.toString();
 	    }
@@ -17,7 +16,7 @@ public class substitution {
 	        StringBuilder sb = new StringBuilder(s.length());
 	 
 	        for (char c : s.toCharArray())
-	            sb.append((char) (key.indexOf((int) c) + 32));
+	            sb.append((char) (key.indexOf((int) c) + 65));
 	 
 	        return sb.toString();
 	    }
